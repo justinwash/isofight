9e241b578cb357d710ccfd267d911d5d7a9dd0c6
Trifectuh <trifectuh@icloud.com>
2017-08-09 00:33:08 -0500

    stock counter
    it works! and the cubemen don't flop around like fish on respawn.

---
+++ Assets/Scripts/HealthCounter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class HealthCounter : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    bool hitbyopponent;
+    bool isDead;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hitbyopponent)
+        {
+            //hit effect etc maybe
+        }
+
+        hitbyopponent = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        hitbyopponent = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            // KAYYYYY OHHHHHH
+            Death();
+        }
+    }
+    void Death()
+    {
+        // Set the death flag, eventually have charlie yell something dumb about Cammy
+        isDead = true;
+    }
+}
---
+++ Assets/Scripts/StockCounter.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class StockCounter : MonoBehaviour
+{
+    public int startingStocks = 6;
+    public int oldStockCount;
+    public int newStockCount;
+    public Rigidbody pleaseStop;
+    Vector3 respawnPosition;
+    Quaternion respawnRotation;
+    public bool isDead;
+
+    // Use this for initialization
+    void Start()
+    {
+        pleaseStop = GetComponent<Rigidbody>();
+        respawnPosition = gameObject.transform.position;
+        respawnRotation = gameObject.transform.rotation;
+        oldStockCount = startingStocks;
+        newStockCount = startingStocks;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y <= -500 && oldStockCount == newStockCount)
+        {
+            FellOff();
+        }
+
+        if (gameObject.transform.position.y <= -1000 && !isDead)
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        // eventually respawn the character on the stage, then set 
+        // oldStockCount the same as newStockCount so FellOff()works next timme we fall off
+        pleaseStop.velocity = Vector3.zero;
+        gameObject.transform.position = respawnPosition;
+        gameObject.transform.rotation = respawnRotation;
+        oldStockCount = newStockCount;
+    }
+
+    public void FellOff()
+    {
+        newStockCount = oldStockCount - 1;
+
+        if (newStockCount <= 0 && !isDead)
+        {
+            // KAYYYYY OHHHHHH
+            Death();
+        }
+
+        
+    }
+    void Death()
+    {
+        // Set the death flag, eventually have charlie yell something dumb about Cammy
+        isDead = true;
+    }
+}
