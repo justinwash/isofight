1325a81296f72926633f7ff2cbd6edc408125c7f
Trifectuh <trifectuh@icloud.com>
2017-08-08 21:01:19 -0500

    Init
    Initial commit

---
+++ Assets/Standard Assets/2D/Scripts/SwitchCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour
+{
+
+    public Transform Capsule1;
+    public Transform Capsule2;
+    public Vector3 center;
+
+    void Update()
+    {
+        center = ((Capsule2.position - Capsule1.position) / 2.0f) + Capsule1.position;
+        center.y = center.y - 0;
+        transform.position = center;
+    }
+}
