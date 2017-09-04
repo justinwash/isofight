d02e922af6da6f14655965299cdbc5e0cb648cc0
Trifectuh <trifectuh@icloud.com>
2017-09-03 20:22:17 -0500

    hitboxes work! (for just the one punch we have)

---
+++ Assets/HitboxManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxManager : MonoBehaviour
+{
+
+    bool hitboxActive;
+
+	void Start()
+	{
+        hitboxActive = false;
+	}
+
+    void OnTriggerStay(Collider col)
+	{
+        if (hitboxActive == true)
+        {
+            hitboxActive = false;
+            Debug.Log(gameObject.name + " hit !");
+        }
+	}
+
+    void ActivateHitbox()
+    {
+        hitboxActive = true;
+    }
+
+    void DeactivateHitbox()
+    {
+        hitboxActive = false;
+    }
+}
