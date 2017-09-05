2b834f94f263acce8d0265f42331125540b883b8
Trifectuh <trifectuh@icloud.com>
2017-09-04 20:16:34 -0500

    more refactoring and breaking stuff

---
+++ Assets/Scripts/JoePunch1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoePunch1 : MonoBehaviour {
+
+    public bool canAttack;
+    public bool canMove;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("p1Punch1") && canAttack == true) // Punch1 if Punch1 is pressed and we're allowed to
+        {
+
+            Punch1();
+        }
+    }
+
+    void Punch1()
+    {
+	    canMove = false;
+	    GetComponentInChildren<Animator>().SetTrigger("Punch1");
+	    canAttack = false;
+    }
+
+    public void Punch1Done()
+    {
+	    GetComponentInChildren<Animator>().ResetTrigger("Punch1");
+    }
+
+    public void ActionReset()
+    {
+	    canMove = true;
+	    canAttack = true;
+    }
+
+    public void AttackDisable()
+    {
+        canAttack = false;
+    }
+}
+
