4b620b0837ba8d47b6778d2a79e2798a9494994e
Trifectuh <trifectuh@icloud.com>
2017-09-07 18:04:08 -0500

    made the timer do the counting thing

---
+++ Assets/RoundTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundTimer : MonoBehaviour {
+
+    static float timer;
+    public bool roundOver;
+
+	// Use this for initialization
+	void Start () {
+        timer = 99;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            GetComponent<Text>().text = timer.ToString("0");
+        }
+        else roundOver = true;
+	}
+}
